868db5df0e70a8d9b1a74400ecbac9271afb07f9
Bilal Houari <bilal.bm.houari@gmail.com>
2026-03-31 21:10:47 +0100

    admin overview stats

--- core/views.py
+++ core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from students.models import Student
+from teachers.models import Teacher
+from departments.models import Department
+from classes.models import SchoolClass
 
 @login_required
 def home(request):
     if request.user.role == 'STUDENT':
         return redirect('student-dashboard')
-    return render(request, 'home.html')
+    
+    context = {
+        'student_count': Student.objects.count(),
+        'teacher_count': Teacher.objects.count(),
+        'dept_count': Department.objects.count(),
+        'class_count': SchoolClass.objects.count(),
+    }
+    return render(request, 'home.html', context)
